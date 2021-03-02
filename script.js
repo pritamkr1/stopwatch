dd40841abba6b4bb8eeafeffcef7aa05f89fa5c6
Pritam Kumar <77887792+pritamkr1@users.noreply.github.com>
2021-03-02 22:32:12 +0530

    pending

---
+++ script.js
@@ -0,0 +1,17 @@
+//Selecting items 
+window.onload = function(){
+let value;
+let secValue = 00;
+let tensValue = 00;
+
+let second = document.querySelector('.sec');
+let tenss = document.querySelector('.tens');
+
+let startBtn = document.querySelector('.btn-start');
+let resetBtn = document.querySelector('.btn-reset');
+let stopBtn = document.querySelector('.btn-stop');
+
+
+
+
+}
